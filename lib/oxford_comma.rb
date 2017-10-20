205ca167f8dead4233200bd249fa150cfe0c2ae6
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 22:02:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array.each do |fruit|
-      "#{fruit} blah"
-    end
-      array.join
+    array[1] += " and"
+    array.join(", ")
   end
 end
 
