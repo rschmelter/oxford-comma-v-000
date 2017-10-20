9bb024bbc65d7e37b9dbee4b35ab9401895de3f2
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 21:20:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    thre_words = array.join(", ")
+    three_words = array.join(", ")
     three_words.split
     three_words[1] += "and"
     three_words.join
