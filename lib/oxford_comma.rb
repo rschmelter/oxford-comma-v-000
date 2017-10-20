c413ef35144a509c86d94dd954c4b9c6cf67eab2
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 20:29:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    array[1] += " and"
+    
     array.join(", ")
+    array[1] += " and"
   end
 end
