2597f198a68f1fe71ace119d7b2a3878f252568c
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-20 21:53:50 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,12 +4,15 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-      array[1] += " and"
-      array.join(", ")
+      array.each do |fruit|
+      "#{fruit},"
+    end
     end
 end
 
-# three_words = array.join(", ")
-# three_words.split
-# three_words[1] += "and"
-# three_words.join
+  # array.each do |fruit|
+  #   "#{fruit},"
+  # end
+  # 
+  # array[1] += " and"
+  # array.join(", ")
